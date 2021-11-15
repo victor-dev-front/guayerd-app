2ceb8a0046f75681d545fadd05f45aad73e16857
Alberto <albertodamianlopez@gmail.com>
2021-11-15 18:29:52 -0300

    Agregado de inputs al contacto

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -14,29 +14,65 @@ const validarTelefono =
 const validationSchema = yup.object({
   nombre: yup
     .string()
-    .matches(validarNombre, "Debe ingresar un nombre válido")
-    .required("El nombre es obligatorio")
-    .max(40, "El nombre no puede tener más de 40 caracteres"),
+    .matches(validarNombre, "* Debe ingresar un nombre válido")
+    .required("* El nombre es obligatorio")
+    .max(40, "* El nombre no puede tener más de 40 caracteres"),
   email: yup
     .string()
-    .required("El mail es obligatorio")
-    .email("Debe ingresar un mail válido")
-    .min(10, "El email ingresado no puede tener menos de 10 caracteres")
-    .max(30, "El email ingresado no puede tener más de 30 caracteres"),
+    .required("* El mail es obligatorio")
+    .email("* Debe ingresar un mail válido")
+    .min(10, "* El email ingresado no puede tener menos de 10 caracteres")
+    .max(30, "* El email ingresado no puede tener más de 30 caracteres"),
   telefono: yup
     .string()
-    .required("El teléfono es obligatorio")
-    .matches(validarTelefono, "Debe ingresar un teléfono válido")
-    .min(10, "El telefono ingresado no puede tener menos de 10 caracteres")
-    .max(30, "El telefono ingresado no puede tener más de 30 caracteres")
+    .required("* El teléfono es obligatorio")
+    .matches(validarTelefono, "* Debe ingresar un teléfono válido")
+    .min(10, "* El teléfono ingresado no puede tener menos de 10 caracteres")
+    .max(30, "* El teléfono ingresado no puede tener más de 30 caracteres"),
+  consulta: yup
+    .string()
+    .required("* Por favor ingrese su consulta")
+    .max(300, "* Su consulta no puede tener más de 300 caracteres")
 });
 
+// Array para input de selección múltiple
+const opciones = [
+  // {
+  //   value: "",
+  //   label: ""
+  // },
+  {
+    value: "venta",
+    label: "Venta"
+  },
+  {
+    value: "trabajo",
+    label: "Trabajo"
+  },
+  {
+    value: "reparacion",
+    label: "Reparación"
+  },
+  {
+    value: "otros",
+    label: "Otros"
+  }
+];
+
 const Form = () => {
+  // const [tema, setTema] = React.useState("Tema");
+
+  // const handleChange = (event) => {
+  //   setTema(event.target.value);
+  // };
+
   const formik = useFormik({
     initialValues: {
       nombre: "",
       email: "",
-      telefono: ""
+      telefono: "",
+      tema: "Venta",
+      consulta: ""
     },
     onSubmit: (values) => {
       console.log(JSON.stringify(values));
@@ -46,7 +82,7 @@ const Form = () => {
 
   return (
     <>
-      <form onSubmit={formik.handleSubmit}>
+      <form onSubmit={formik.handleSubmit} autoComplete="off">
         <Container>
           <TextField
             fullWidth="30"
@@ -54,10 +90,21 @@ const Form = () => {
             name="nombre"
             label="Nombre"
             margin="normal"
+            variant="outlined"
+            size="small"
             values={formik.values.nombre}
             onChange={formik.handleChange}
             error={formik.touched.nombre && Boolean(formik.errors.nombre)}
             helperText={formik.touched.nombre && formik.errors.nombre}
+            FormHelperTextProps={{ style: { fontSize: 12 } }}
+            inputProps={{ style: { fontSize: 15 } }} // font size of input text
+            InputLabelProps={{
+              style: {
+                paddingTop: 3,
+                fontSize: 15,
+                color: "grey"
+              }
+            }} // font size of input label
           />
           <br />
           <br />
@@ -67,10 +114,21 @@ const Form = () => {
             name="email"
             label="Email"
             margin="normal"
+            variant="outlined"
+            size="small"
             values={formik.values.email}
             onChange={formik.handleChange}
             error={formik.touched.email && Boolean(formik.errors.email)}
             helperText={formik.touched.nombre && formik.errors.email}
+            FormHelperTextProps={{ style: { fontSize: 12 } }}
+            inputProps={{ style: { fontSize: 15 } }} // font size of input text
+            InputLabelProps={{
+              style: {
+                paddingTop: 3,
+                fontSize: 15,
+                color: "grey"
+              }
+            }} // font size of input label
           />
           <br />
           <br />
@@ -78,13 +136,95 @@ const Form = () => {
             fullWidth="30"
             id="telefono"
             name="telefono"
-            label="Telefono"
+            label="Teléfono"
             margin="normal"
+            variant="outlined"
+            size="small"
             values={formik.values.telefono}
             onChange={formik.handleChange}
             error={formik.touched.telefono && Boolean(formik.errors.telefono)}
             helperText={formik.touched.nombre && formik.errors.telefono}
+            FormHelperTextProps={{
+              style: { fontSize: 12 }
+            }}
+            inputProps={{ style: { fontSize: 15 } }} // font size of input text
+            InputLabelProps={{
+              style: {
+                paddingTop: 3,
+                fontSize: 15,
+                color: "grey"
+              }
+            }} // font size of input label
           />
+          <br />
+          <br />
+          <TextField
+            fullWidth="30"
+            id="tema"
+            select
+            label="Tema"
+            // value={tema}
+            // onChange={handleChange}
+            values={formik.values.tema}
+            onChange={formik.handleChange}
+            error={formik.touched.tema && Boolean(formik.errors.tema)}
+            helperText={formik.touched.tema && formik.errors.tema}
+            FormHelperTextProps={{
+              style: { fontSize: 12 }
+            }}
+            SelectProps={{
+              native: true
+            }}
+            variant="outlined"
+            margin="normal"
+            size="small"
+            inputProps={{ style: { fontSize: 12 } }} // font size of input text
+            InputLabelProps={{
+              style: {
+                paddingTop: 3,
+                fontSize: 15,
+                color: "grey"
+              }
+            }} // font size of input label
+          >
+            {opciones.map((option) => (
+              <option key={option.value} value={option.value}>
+                {option.label}
+              </option>
+            ))}
+          </TextField>
+          <br />
+          <br />
+          <TextField
+            fullWidth="30"
+            id="consulta"
+            label="Ingrese su consulta"
+            // placeholder="Ingrese su consulta"
+            multiline
+            rows={7}
+            values={formik.values.consulta}
+            onChange={formik.handleChange}
+            error={formik.touched.consulta && Boolean(formik.errors.consulta)}
+            helperText={formik.touched.consulta && formik.errors.consulta}
+            FormHelperTextProps={{
+              style: { fontSize: 12 }
+            }}
+            SelectProps={{
+              native: true
+            }}
+            variant="outlined"
+            margin="normal"
+            size="normal"
+            inputProps={{ style: { fontSize: 15 } }} // font size of input text
+            InputLabelProps={{
+              style: {
+                paddingTop: 3,
+                fontSize: 15,
+                color: "grey"
+              }
+            }} // font size of input label
+          />
+          <br />
           <br />
           <br />
           <Button
@@ -92,6 +232,7 @@ const Form = () => {
             type="submit"
             variant="outlined"
             color="default"
+            size="normal"
           >
             Enviar
           </Button>
--- src/components/section/Section.js
+++ src/components/section/Section.js
@@ -2,27 +2,50 @@ import { Container } from "react-bootstrap";
 import styles from "./Section.module.css";
 import { BsCheck2Square } from "react-icons/bs";
 const Section = () => {
-    return ( 
-        <div className={styles.section}>
-            <Container>
-               <h2 className={styles.subTitulo}>¿Por que elegirnos?</h2>
-               <ul>
-                    <li>
-                        <p>Te brindaremos un asesoramiento de primer nivel. <span className={styles.colorIcono}><BsCheck2Square/></span></p>
-                    </li>
-                    <li>
-                        <p>Nuestros productos se exportan y se venden en 77 paises. <span className={styles.colorIcono}><BsCheck2Square/></span></p>
-                    </li>
-                    <li>
-                        <p>Cada bicicleta que se ofrece al publico esta echa con materia prima de primera calidad. <span className={styles.colorIcono}><BsCheck2Square/></span> </p>
-                    </li>
-                    <li>
-                        <p>En GUAYERD nos aseguramos que cada bicicleta que esta en camino a su nuevo dueño, sea unica y tengo el mayor cuidado en su fabricacion. <span className={styles.colorIcono}><BsCheck2Square/></span></p>
-                    </li>
-               </ul>
-             </Container>
-        </div>
-     );
-}
- 
+  return (
+    <div className={styles.section}>
+      <Container>
+        <h2 className={styles.subTitulo}>¿Por qué elegirnos?</h2>
+        <ul>
+          <li>
+            <p>
+              Te brindaremos un asesoramiento de primer nivel.{" "}
+              <span className={styles.colorIcono}>
+                <BsCheck2Square />
+              </span>
+            </p>
+          </li>
+          <li>
+            <p>
+              Nuestros productos se exportan y se venden en 77 países.{" "}
+              <span className={styles.colorIcono}>
+                <BsCheck2Square />
+              </span>
+            </p>
+          </li>
+          <li>
+            <p>
+              Cada bicicleta que se ofrece al público esta hecha con materia
+              prima de primera calidad.{" "}
+              <span className={styles.colorIcono}>
+                <BsCheck2Square />
+              </span>{" "}
+            </p>
+          </li>
+          <li>
+            <p>
+              En GUAYERD nos aseguramos que cada bicicleta que está en camino a
+              su nuevo dueño, sea única y tenga el mayor cuidado en su
+              fabricación.{" "}
+              <span className={styles.colorIcono}>
+                <BsCheck2Square />
+              </span>
+            </p>
+          </li>
+        </ul>
+      </Container>
+    </div>
+  );
+};
+
 export default Section;
