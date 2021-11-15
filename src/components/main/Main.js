98f8409163466735fe6f4d1b57cd98809991b319
victor <vgarcia164@alumnos.iua.edu.ar>
2021-11-15 19:11:37 -0300

    productos en desarrollo

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -5,46 +5,63 @@ import {Carousel,Container} from "react-bootstrap";
 const Main = () => {
   
     const {productos} = useContext(DataContext);
+    console.log(productos);
     return ( 
-
           <Container>
             <Carousel>
-                <Carousel.Item interval={1000}>
-                    <img
-                    className="d-block w-100"
-                   // src=
-                    alt="First slide"
-                    />
-                    <Carousel.Caption>
-                    <h3>First slide label</h3>
-                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item interval={500}>
-                    <img
-                    className="d-block w-100"
-                    src="holder.js/800x400?text=Second slide&bg=282c34"
-                    alt="Second slide"
-                    />
-                    <Carousel.Caption>
-                    <h3>Second slide label</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item>
-                    <img
-                    className="d-block w-100"
-                    src="holder.js/800x400?text=Third slide&bg=20232a"
-                    alt="Third slide"
-                    />
-                    <Carousel.Caption>
-                    <h3>Third slide label</h3>
-                    <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
+                {
+                 productos.map((producto,i) => {
+                     if(i === 0){
+                         return (
+                            <Carousel.Item interval={1000}>
+                            <img
+                            className="d-block w-100"
+                            src={producto.imgUrl}
+                            alt={producto.title}
+                            />
+                                <Carousel.Caption>
+                                <h3>{producto.title}</h3>
+                                <button>VER MAS</button>
+                                </Carousel.Caption>
+                            </Carousel.Item>  
+                         );
+                     } else if(i === 1){
+                         return (
+                            <Carousel.Item interval={500}>
+                            <img
+                            className="d-block w-100"
+                            src={producto.imgUrl}
+                            alt={producto.title}
+                            />
+                                <Carousel.Caption>
+                                <h3>{producto.title}</h3>
+                                <button>VER MAS</button>
+                                </Carousel.Caption>
+                            </Carousel.Item>    
+                         );
+                     } else if(i === 2){
+                         return (
+                            <Carousel.Item>
+                            <img
+                            className="d-block w-100"
+                            src={producto.imgUrl}
+                            alt={producto.title}
+                            />
+                                <Carousel.Caption>
+                                <h3>{producto.title}</h3>
+                                <button>VER MAS</button>
+                                </Carousel.Caption>
+                            </Carousel.Item>
+                         );
+                     }
+                 }
+            
+                 )    
+                }
             </Carousel>
          </Container>
      );
 }
  
 export default Main;
+
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,12 +1,14 @@
 import Header from "../../components/header/Header";
 import Section from "../../components/section/Section";
 import { Container } from "react-bootstrap";
+import Main from "../../components/main/Main";
 
 
  const Home = () => {
      return (
          <> 
             <Header />
+            <Main />
             <Section /> 
          </>
       );
