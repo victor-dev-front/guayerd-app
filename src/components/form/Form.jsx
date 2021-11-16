19728bbf6d56a66fb080a71234eb8f4f49a47553
Alberto <albertodamianlopez@gmail.com>
2021-11-16 19:17:43 -0300

    agregado de imagen de fondo y estilos

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -112,12 +112,16 @@ const Form = () => {
             FormHelperTextProps={{
               style: { fontSize: 12, fontWeight: "bold" }
             }}
-            inputProps={{ style: { fontSize: 15 } }} // font size of input text
+            inputProps={{
+              style: { fontSize: 15, fontWeight: "bold" }
+            }} // font size of input text
             InputLabelProps={{
               style: {
                 paddingTop: 3,
                 fontSize: 15,
-                color: "grey"
+                // color: "grey",
+                color: "black",
+                fontWeight: "bold"
               }
             }} // font size of input label
           />
@@ -135,12 +139,13 @@ const Form = () => {
             FormHelperTextProps={{
               style: { fontSize: 12, fontWeight: "bold" }
             }}
-            inputProps={{ style: { fontSize: 15 } }} // font size of input text
+            inputProps={{ style: { fontSize: 15, fontWeight: "bold" } }} // font size of input text
             InputLabelProps={{
               style: {
                 paddingTop: 3,
                 fontSize: 15,
-                color: "grey"
+                color: "black",
+                fontWeight: "bold"
               }
             }}
           />
@@ -158,12 +163,13 @@ const Form = () => {
             FormHelperTextProps={{
               style: { fontSize: 12, fontWeight: "bold" }
             }}
-            inputProps={{ style: { fontSize: 15 } }}
+            inputProps={{ style: { fontSize: 15, fontWeight: "bold" } }}
             InputLabelProps={{
               style: {
                 paddingTop: 3,
                 fontSize: 15,
-                color: "grey"
+                color: "black",
+                fontWeight: "bold"
               }
             }}
           />
@@ -186,12 +192,13 @@ const Form = () => {
             variant="outlined"
             margin="normal"
             size="small"
-            inputProps={{ style: { fontSize: 12 } }}
+            inputProps={{ style: { fontSize: 12, fontWeight: "bold" } }}
             InputLabelProps={{
               style: {
                 paddingTop: 3,
                 fontSize: 15,
-                color: "grey"
+                color: "black",
+                fontWeight: "bold"
               }
             }}
           >
@@ -220,13 +227,14 @@ const Form = () => {
             margin="normal"
             size="normal"
             inputProps={{
-              style: { fontSize: 15, lineHeight: "100%" }
+              style: { fontSize: 15, lineHeight: "100%", fontWeight: "bold" }
             }}
             InputLabelProps={{
               style: {
                 paddingTop: 3,
                 fontSize: 15,
-                color: "grey"
+                color: "black",
+                fontWeight: "bold"
               }
             }}
           />
@@ -258,21 +266,22 @@ const Form = () => {
             </Row>
           </Container>
         </form>
-        <Container>
-          <h2 className={style.mapa}>
-            <span className={style.ubicacion}>Dónde encontrarnos!</span>
-          </h2>
-          <div className={style.map__responsive}>
-            <iframe
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3284.512780793885!2d-58.4500563851957!3d-34.59119276434157!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bcb5f12e398ecd%3A0x98abf435fbfab70!2sAv.%20Corrientes%206237%2C%20C1427BPA%20CABA!5e0!3m2!1ses-419!2sar!4v1626215769377!5m2!1ses-419!2sar"
-              title="Mapa"
-              width="600"
-              height="450"
-              loading="lazy"
-            ></iframe>
-          </div>
-        </Container>
       </Grid>
+      <div className={style.container__mapa}>
+        <h2 className={style.mapa}>
+          <span className={style.ubicacion}>Dónde encontrarnos!</span>
+        </h2>
+        <div className={style.map__responsive}>
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3284.512780793885!2d-58.4500563851957!3d-34.59119276434157!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bcb5f12e398ecd%3A0x98abf435fbfab70!2sAv.%20Corrientes%206237%2C%20C1427BPA%20CABA!5e0!3m2!1ses-419!2sar!4v1626215769377!5m2!1ses-419!2sar"
+            title="Mapa"
+            width="600"
+            height="450"
+            loading="lazy"
+          ></iframe>
+        </div>
+      </div>
+      {/* </Grid> */}
     </>
   );
 };
