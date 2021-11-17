39128080d0bf3013817dec5023e3cb426c1b18fc
Alberto <albertodamianlopez@gmail.com>
2021-11-17 14:42:36 -0300

    centrado de botones en card y modal de productos

--- src/components/ProductosDetalles/ProductosDetalles.jsx
+++ src/components/ProductosDetalles/ProductosDetalles.jsx
@@ -1,10 +1,9 @@
 import React, { useState } from "react";
 import { Button, Modal } from "react-bootstrap";
 
-
 //Componente de la ventana modal que muestra los detalles de cada producto
 
-const ProductosDetalles = ({img, description, title }) => {
+const ProductosDetalles = ({ img, description, title }) => {
   const [show, setShow] = useState(false);
 
   const handleClose = () => setShow(false);
@@ -12,17 +11,33 @@ const ProductosDetalles = ({img, description, title }) => {
 
   return (
     <>
-      <Button  size="lg" variant="primary" onClick={handleShow}>
-        Ver detalles del articulo
-      </Button>
-
-      <Modal aria-labelledby="contained-modal-title-vcenter" show={show} onHide={handleClose} animation={true}>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center"
+        }}
+      >
+        <Button size="lg" variant="primary" onClick={handleShow}>
+          Ver detalles del articulo
+        </Button>
+      </div>
+      <Modal
+        aria-labelledby="contained-modal-title-vcenter"
+        show={show}
+        onHide={handleClose}
+        animation={true}
+      >
         <Modal.Header closeButton>
           <Modal.Title> {title} </Modal.Title>
         </Modal.Header>
         <img className="img" src={img} alt={title} />
         <Modal.Body> {description} </Modal.Body>
-        <Modal.Footer>
+        <Modal.Footer
+          style={{
+            display: "flex",
+            justifyContent: "center"
+          }}
+        >
           <Button variant="primary" onClick={handleClose}>
             Cerrar
           </Button>
