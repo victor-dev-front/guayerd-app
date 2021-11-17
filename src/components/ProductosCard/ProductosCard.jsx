7d08de15771dd878652acfb0ce70de52f8574a2c
victor <vgarcia164@alumnos.iua.edu.ar>
2021-11-17 19:46:24 -0300

    navbar terminado

--- src/components/ProductosCard/ProductosCard.jsx
+++ src/components/ProductosCard/ProductosCard.jsx
@@ -25,10 +25,10 @@ const ProductosCard = ({
               <img className="img" src={img} alt={title} />
               <div className="card-body">
                 <h3 className={style.title}> {title} </h3>
-                <h5 className={discount ? style.tachado : style.price }>  {price} {currency} </h5>
+                <h5 className={discount ? style.tachado : style.price } >  {price} {currency} </h5>
                 <h4 className={style.descuento}> {discount  ?? discount} {discount ? currency : null} </h4>
                 <h5 className={style.stock}> Disponibles: {inStock} </h5>
-                <ProductoDetalles description={description} title={title} img={img} />
+                <ProductoDetalles description={description} title={title} img={img}/>
               </div>
             </Card>
           </Col>
