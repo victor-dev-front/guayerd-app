b64e82b1c5ef25804fffc217ad399f4cb33e738a
victor <vgarcia164@alumnos.iua.edu.ar>
2021-11-18 19:03:50 -0300

    stylos pagina nosotros

--- src/pages/nosotros/Nosotros.jsx
+++ src/pages/nosotros/Nosotros.jsx
@@ -2,14 +2,17 @@ import {Container} from "react-bootstrap";
 import Exitos from "../../components/guayerd/exitos/Exitos";
 import Historia from "../../components/guayerd/historia/Historia";
 import Innovacion from "../../components/guayerd/innovacion/Innovacion";
+import styles from "./Nosotros.module.css";
 
 const Nosotros = () => {
-    return ( 
-        <Container>
-          <Historia/>
-          <Innovacion/>
-          <Exitos />
-        </Container>
+    return (
+        <div  className={styles.fondo}>
+          <Container>
+            <Historia/>
+            <Innovacion/>
+            <Exitos />
+          </Container>
+        </div>
      );
 }
  
